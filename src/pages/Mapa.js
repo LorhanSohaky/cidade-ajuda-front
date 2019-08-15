034fee47e2291808c6c05df4122c8c52304fc360
LorhanSohaky <lorhansohaky@gmail.com>
2019-08-15 19:55:48 -0300

    Criada a página de relatório e mapa

---
+++ src/pages/Mapa.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Mapa({history}) {
+  return (
+    <div className="mapa-container">
+		<div></div>
+      aaaa
+
+    </div>
+  );
+}
---
+++ src/pages/Relatorio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Relatorio({history}) {
+  return (
+    <div className="relatorio-container">
+		
+      bbbbb
+
+    </div>
+  );
+}
