56777fb8ee8486cd4d1c84f39e6cf465bc402748
LorhanSohaky <lorhansohaky@gmail.com>
2019-08-17 02:19:56 -0300

    Criada página para relatar ocorrência

---
+++ src/pages/NovaOcorrencia/NovaOcorrencia.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Done from '@material-ui/icons/Done';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
+
+export default function NovaOcorrencia({ history }) {
+	const classes = useStyles();
+
+	function handleBackMaps() {
+		history.push('/');
+	}
+
+	function save() {
+
+	}
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="voltar" onClick={handleBackMaps}>
+						<ArrowBack />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>Nova ocorrência</Typography>
+					<IconButton edge="end" color="inherit" aria-label="voltar" onClick={save}>
+						<Done />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
+import NovaOcorrencia from './pages/NovaOcorrencia/NovaOcorrencia';
+
 
 export default function Routes() {
 	return (
 		<BrowserRouter>
 			<Route path="/" exact component={App} />
+			<Route path="/nova-ocorrencia" component={NovaOcorrencia} />
 		</BrowserRouter>
 	);
 }
