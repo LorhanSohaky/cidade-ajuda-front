52902cc2c88c7b5c5b96bcc83a94072edc35c9c5
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-02 10:35:39 -0300

    Renomear página

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-import NovaOcorrencia from '../pages/NovaOcorrencia/NovaOcorrencia'
+import UpsertIncident from '../pages/UpsertIncident'
 import paths from './paths'
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route path={paths.home} exact component={Home} />
-      <Route path={paths.incident.create} component={NovaOcorrencia} />
+      <Route path={paths.incident.create} component={UpsertIncident} />
     </BrowserRouter>
   )
 }
