49c0a594eb4dded064f5b0db3555e9f58e82ccfc
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-09 18:34:02 -0300

    Add the private route component

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,48 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import Home from '../pages/Home'
 import UpsertIncident from '../pages/UpsertIncident'
 import paths from './paths'
+import { useSelector, useDispatch } from 'react-redux'
+import settingsActions from '../redux/settings'
 
-const Routes = () => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = !!useSelector(state => state.userState.token)
+  const dispatch = useDispatch()
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (isAuthenticated) {
+          return <Component {...props} />
+        } else {
+          dispatch(settingsActions.setNavigationTab('profile'))
+          return (
+            <Route
+              {...rest}
+              render={props => (
+                <Redirect
+                  to={{ pathname: paths.home, state: { from: props.location } }}
+                />
+              )}
+            />
+          )
+        }
+      }}
+    />
+  )
+}
+
+const Routes = ({ isAuthenticated }) => {
   return (
     <BrowserRouter>
       <Route path={paths.home} exact component={Home} />
-      <Route path={paths.incident.create} component={UpsertIncident} />
+      <PrivateRoute
+        path={paths.incident.create}
+        component={UpsertIncident}
+        isAuthenticated={isAuthenticated}
+      />
     </BrowserRouter>
   )
 }
