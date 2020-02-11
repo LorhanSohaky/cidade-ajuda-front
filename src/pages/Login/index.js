8978bbd1f1dffb9885c4a8ef042b91125db81776
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-11 17:59:15 -0300

    Show toast when logged in and when an error occurs

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Box, TextField, Button } from '@material-ui/core'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import logo from '../../assets/logo.png'
 import API from '../../services/api'
 import userActions from '../../redux/user'
+import { ToastContext } from '../../services/ToastHandler'
 
 function Login ({ setToken, setMe }) {
   const [fields, setFields] = useState({})
+
+  const toastContext = useContext(ToastContext)
 
   const handleEvent = event => {
     setFields({ ...fields, [event.target.name]: event.target.value })
@@ -22,8 +25,16 @@ function Login ({ setToken, setMe }) {
       })
       .then(response => {
         setMe(response.data)
+        toastContext.setMessage('Logado com sucesso!')
+        toastContext.setSeverity('success')
+        toastContext.setIsOpen(true)
       })
-      .catch(err => console.error(err))
+      .catch(err => {
+        console.error(err)
+        toastContext.setMessage('Não foi possível logar!')
+        toastContext.setSeverity('error')
+        toastContext.setIsOpen(true)
+      })
   }
 
   return (
