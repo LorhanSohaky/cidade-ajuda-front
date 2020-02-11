aef0ca01ebe5368671ccebf98b7af870c2bb0b4b
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-10 23:02:15 -0300

    Display toast when access a private route without credentials

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import Home from '../pages/Home'
 import UpsertIncident from '../pages/UpsertIncident'
 import paths from './paths'
 import { useSelector, useDispatch } from 'react-redux'
 import settingsActions from '../redux/settings'
+import { ToastContext } from '../services/ToastHandler'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = !!useSelector(state => state.userState.token)
   const dispatch = useDispatch()
+  const toastContext = useContext(ToastContext)
 
   return (
     <Route
@@ -18,6 +20,12 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
           return <Component {...props} />
         } else {
           dispatch(settingsActions.setNavigationTab('profile'))
+          toastContext.setMessage(
+            'Para acessar essa funcionalidade é necessário estar logado'
+          )
+          toastContext.setSeverity('warning')
+          toastContext.setIsOpen(true)
+
           return (
             <Route
               {...rest}
