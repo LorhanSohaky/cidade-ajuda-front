dd06198f40b0ee1da12737e4318c4b3b4f4fbade
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-15 21:56:54 -0300

    Get incidents from api and set the markers

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
 
 import { Box, Fab } from '@material-ui/core'
 import { styled } from '@material-ui/core/styles'
 import AddIcon from '@material-ui/icons/Add'
 
-import { Map as LeafMap, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+import { Map as LeafMap, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import { paths } from '../../routes'
-import { connect } from 'react-redux'
+import API from '../../services/api'
 
 const DEFAULT_VIEWPORT = {
   center: [51, -0.09],
@@ -20,6 +20,34 @@ export function Map ({ history, coords, dimensions }) {
   const [smaller, setSmaller] = React.useState(false)
   const [loadedLocation, setLoadedLocation] = React.useState(false)
   const [viewport, setViewport] = React.useState(DEFAULT_VIEWPORT)
+  const [markers, setMarkers] = React.useState([])
+
+  const mapRef = useRef(null)
+
+  function handleBounds (element) {
+    const bounds = element.getBounds()
+    const southWest = Object.values(bounds._southWest)
+    const northEast = Object.values(bounds._northEast)
+
+    API.listIncidents({ southWest, northEast })
+      .then(response => {
+        const incidents = response.data.results.map(
+          ({ id, latitude, longitude, descricao }) => ({
+            id,
+            position: [latitude, longitude],
+            descricao
+          })
+        )
+        setMarkers(incidents)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  useEffect(() => {
+    handleBounds(mapRef.current.leafletElement)
+  }, [])
 
   useEffect(() => {
     if (coords && !loadedLocation) {
@@ -43,6 +71,15 @@ export function Map ({ history, coords, dimensions }) {
     history.push(paths.incident.create)
   }
 
+  const Markers = () =>
+    markers.map(item => {
+      return (
+        <Marker key={`marker-${item.id}`} position={item.position}>
+          <Popup>{item.descricao}</Popup>
+        </Marker>
+      )
+    })
+
   return (
     <Box flex={1}>
       <AddButton
@@ -53,17 +90,21 @@ export function Map ({ history, coords, dimensions }) {
       >
         <AddIcon />
       </AddButton>
-
       <LeafMap
         style={{ height: '100%' }}
         maxZoom={19}
-        onViewportChanged={newViewport => setViewport(newViewport)}
+        ref={mapRef}
+        onViewportChanged={newViewport =>
+          newViewport && setViewport(newViewport)
+        }
         viewport={viewport}
+        onMoveend={event => handleBounds(event.target)}
       >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+        <Markers />
       </LeafMap>
     </Box>
   )
