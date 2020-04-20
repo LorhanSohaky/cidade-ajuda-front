1e838a8abcff932325a82b130fe7ca306eb3130c
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-04-20 18:03:19 -0300

    Fix design on desktop

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -17,52 +17,65 @@ function User () {
   }
 
   return (
-    <Box m={2} flex={1} display='flex' flexDirection='column' maxWidth={768}>
-      <TextField
-        style={{ marginTop: 16 }}
-        label='Nome completo'
-        value={`${user.primeiro_nome} ${user.sobrenome}`}
-        variant='outlined'
-        InputProps={{
-          readOnly: true
-        }}
-      />
-      <TextField
-        style={{ marginTop: 16 }}
-        label='Apelido'
-        value={user.apelido}
-        variant='outlined'
-        InputProps={{
-          readOnly: true
-        }}
-      />
-      <TextField
-        style={{ marginTop: 16 }}
-        label='E-mail'
-        value={user.email}
-        variant='outlined'
-        InputProps={{
-          readOnly: true
-        }}
-      />
-      <TextField
-        style={{ marginTop: 16 }}
-        label='Data de nascimento'
-        value={user.data_nascimento}
-        variant='outlined'
-        InputProps={{
-          readOnly: true
-        }}
-      />
-      <Box flex={1} display='flex' alignItems='center' justifyContent='center'>
-        <Button
-          color='primary'
-          variant='contained'
-          style={{ flex: 1 }}
-          onClick={handleClick}
+    <Box flex={1} display='flex' flexDirection='column' alignItems='center'>
+      <Box
+        flex={1}
+        display='flex'
+        flexDirection='column'
+        width='100%'
+        maxWidth={768}
+      >
+        <TextField
+          style={{ marginTop: 16 }}
+          label='Nome completo'
+          value={`${user.primeiro_nome} ${user.sobrenome}`}
+          variant='outlined'
+          InputProps={{
+            readOnly: true
+          }}
+        />
+        <TextField
+          style={{ marginTop: 16 }}
+          label='Apelido'
+          value={user.apelido}
+          variant='outlined'
+          InputProps={{
+            readOnly: true
+          }}
+        />
+        <TextField
+          style={{ marginTop: 16 }}
+          label='E-mail'
+          value={user.email}
+          variant='outlined'
+          InputProps={{
+            readOnly: true
+          }}
+        />
+        <TextField
+          style={{ marginTop: 16 }}
+          label='Data de nascimento'
+          value={user.data_nascimento}
+          variant='outlined'
+          InputProps={{
+            readOnly: true
+          }}
+        />
+        <Box
+          flex={1}
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
         >
-          Sair
-        </Button>
+          <Button
+            color='primary'
+            variant='contained'
+            style={{ flex: 1 }}
+            onClick={handleClick}
+          >
+            Sair
+          </Button>
+        </Box>
       </Box>
     </Box>
   )
